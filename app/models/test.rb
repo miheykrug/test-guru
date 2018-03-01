ae649d823199fe9697d5f7bdcea3727fde881054
Mikhail Kruglik <miheykrug@mail.ru>
2018-03-01 20:39:57 +0300

    add category_tests method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+  def self.category_tests(category)
+    Test.joins('
+      JOIN categories
+      ON category_id = categories.id')
+      .where('categories.title = :category', category: category)
+      .order(title: :desc)
+  end
 end
