c857dbb04bde607e7afaa9ca5a4270bb365e8155
Mikhail Kruglik <miheykrug@mail.ru>
2018-03-01 10:18:03 +0300

    fix categories, tests, questions and answers migrations

--- db/migrate/20180301070221_create_tests.rb
+++ db/migrate/20180301070221_create_tests.rb
@@ -1,9 +1,9 @@
 class CreateTests < ActiveRecord::Migration[5.0]
   def change
     create_table :tests do |t|
-      t.string :title
-      t.integer :level
-      t.integer :category_id
+      t.string :title, null: false
+      t.integer :level, default: 0
+      t.references :category, foreign_key: true
 
       t.timestamps
     end
