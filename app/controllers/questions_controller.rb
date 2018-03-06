b5efc05e978ece1e038bc2a0dd3d2497142c1f87
Mikhail Kruglik <miheykrug@mail.ru>
2018-03-06 23:41:26 +0300

    Lesson 7: fix some mistakes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,32 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i(index create new)
+  before_action :find_question, only: %i(show destroy)
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     questions = @test.questions
     render plain: questions.inspect
   end
 
   def show
-    question = Question.find(params[:id])
-    render plain: question.inspect
+    render plain: @question.inspect
   end
 
   def new
-
   end
 
   def create
-    question = Question.create(question_params)
+    question = @test.questions.build(question_params)
+    question.save
 
-    render plain: question.inspect
+    # render plain: question.inspect
+    redirect_to test_questions_url
   end
 
   def destroy
-    Question.find(params[:id]).destroy
-    render plain: 'Вопрос удален'
+    @question.destroy
+    redirect_to test_questions_url
   end
 
   private
@@ -33,8 +35,12 @@ class QuestionsController < ApplicationController
     @test = Test.find(params[:test_id])
   end
 
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
   def question_params
-    params.permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
